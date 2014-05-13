5a6a32984828934f641939bd1c797ffa9aa3dc0d
doomedraven <bilash@meta.ua>
2014-05-13 19:17:02 +0200

    Update compile.sh

--- Common scripts/compile.sh
+++ Common scripts/compile.sh
@@ -9,4 +9,7 @@ ld -o $1 $1.o
 echo '[+] Your Shellcode:'
 echo $(objdump -d ./$1|grep '[0-9a-f]:'|grep -v 'file'|cut -f2 -d:|cut -f1-6 -d' '|tr -s ' '|tr '\t' ' '|sed 's/ $//g'|sed 's/ /\\x/g'|paste -d '' -s |sed 's/^/"/'|sed 's/$/"/g')
 
+echo '[+] To compile it, execute'
+echo 'gcc -ggdb -z execstack -fno-stack-protector shellcode.c -o shellcode'
+
 echo '[+] Done!'
