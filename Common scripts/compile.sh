6435b32994825c1dddba0aa597cd4f347830e8c0
DoomedRaven <bilash@meta.ua>
2014-05-24 14:29:40 +0200

    init

--- Common scripts/compile.sh
+++ Common scripts/compile.sh
@@ -7,7 +7,7 @@ echo '[+] Linking ...'
 ld -o $1 $1.o
 
 echo '[+] Your Shellcode:'
-echo $(objdump -d ./$1|grep '[0-9a-f]:'|grep -v 'file'|cut -f2 -d:|cut -f1-6 -d' '|tr -s ' '|tr '\t' ' '|sed 's/ $//g'|sed 's/ /\\x/g'|paste -d '' -s |sed 's/^/"/'|sed 's/$/"/g')
+echo for i in $(objdump -d bind | grep '^ ' |cut -f2); do echo -n "\x"$i; done;echo
 
 echo '[+] To compile it, execute'
 echo 'gcc -ggdb -z execstack -fno-stack-protector shellcode.c -o shellcode'
