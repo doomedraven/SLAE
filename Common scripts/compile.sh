aa8a05d94e9f09e3fea411dcb83fe7d6770b8da0
DoomedRaven <bilash@meta.ua>
2014-05-25 16:36:21 +0200

    init

--- Common scripts/compile.sh
+++ Common scripts/compile.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 echo '[+] Assembling with Nasm ... '
-nasm -f elf32 -o $1.o $1.nasm
+nasm -f $2 -o $1.o $1.nasm
 
 echo '[+] Linking ...'
 ld -o $1 $1.o
 
 echo '[+] Your Shellcode:'
-echo for i in $(objdump -d $1 | grep '^ ' |cut -f2); do echo -n "\x"$i; done;echo
+for i in $(objdump -d $1 | grep '^ ' |cut -f2); do echo -n "\x"$i; done;echo
 
 echo '[+] To compile it, execute'
 echo 'gcc -ggdb -z execstack -fno-stack-protector shellcode.c -o shellcode'
